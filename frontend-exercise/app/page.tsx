2f1648c1ee141d30aa5c3a0f1a537cf265f150bb
Ivailo2707 <Ivailo_27@abv.bg>
2024-03-09 14:46:57 +0200

    created next app

---
+++ frontend-exercise/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <a className="mr-5">Signup</a>
+    <a className="ml-5">Login</a>
+    <a className="ml-5">News</a>
+    </>
+  );
+}
